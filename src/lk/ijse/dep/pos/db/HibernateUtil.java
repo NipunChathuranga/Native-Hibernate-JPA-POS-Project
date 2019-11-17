ad9f65b9c6bd684da0b5291dc163f649133a7078
NipunChathuranga <nipunc394@gmail.com>
2019-11-17 20:56:12 +0530

    Add encode and decode to login credentials

--- src/lk/ijse/dep/pos/db/HibernateUtil.java
+++ src/lk/ijse/dep/pos/db/HibernateUtil.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep.pos.db;
 
 
+import lk.ijse.dep.crypto.DEPCrypt;
 import lk.ijse.dep.pos.entity.Customer;
 import lk.ijse.dep.pos.entity.Item;
 import lk.ijse.dep.pos.entity.Order;
@@ -14,28 +15,54 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HibernateUtil {
+
+    private static String username;
+    private static String password;
+    private static String database;
+    private static String host;
+    private static String port;
 
     private static SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
 
         // (1)
-        File file = new File("resources/application.properties");
-//
-//        try(FileInputStream fis = new FileInputStream(p)) {
-//
-//        }
+        File propFile = new File("resources/application.properties");
+        Properties properties = new Properties();
 
+        try (FileInputStream fis = new FileInputStream(propFile)) {
+            properties.load(fis);
+        } catch (Exception e) {
+            Logger.getLogger("lk.ijse.dep.pos.db.HibernateUtil").log(Level.SEVERE, null, e);
+            System.exit(2);
+        }
 
-        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
-//                .configure().build();
-                .loadProperties(file)
+        username = DEPCrypt.decode(properties.getProperty("hibernate.connection.username"), "dep4");
+        password = DEPCrypt.decode(properties.getProperty("hibernate.connection.password"), "dep4");
+        host = properties.getProperty("ijse.dep.ip");
+        database = properties.getProperty("ijse.dep.db");
+        port = properties.getProperty("ijse.dep.port");
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .loadProperties(propFile)
+                .applySetting("hibernate.connection.username",
+                        username)
+                .applySetting("hibernate.connection.password",
+                        password)
                 .build();
 
+//        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+////                .configure().build();
+//                .loadProperties(propFile)
+//                .build();
+
         // (2)
-        Metadata metadata = new MetadataSources(standardRegistry)
+        Metadata metadata = new MetadataSources(registry)
 
                 .addAnnotatedClass(Customer.class)
                 .addAnnotatedClass(Order.class)
@@ -50,6 +77,27 @@ public class HibernateUtil {
         // (3)
         return metadata.getSessionFactoryBuilder()
                 .build();
+    }
+
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static String getDatabase() {
+        return database;
+    }
+
+    public static String getPort() {
+        return port;
+    }
+
+    public static String getHost() {
+        return host;
     }
 
     public static SessionFactory getSessionFactory() {
